66a992ba41a14f418c3c24bf37b10efd9c4dfa2c
joaosobanski <54563534+joaosobanski@users.noreply.github.com>
2022-08-14 13:43:28 -0300

    Connect and deploy on Ethereum networks

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "3f6e2b3b9a134a6497b0db975d71c810";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
+
+  /*
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  */
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     loc_development_development: {
       network_id: "*",
       port: 8545,
@@ -13,3 +37,4 @@ module.exports = {
     }
   }
 };
+
